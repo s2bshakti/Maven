65e869507d734ed8c4e0d022c8eafcccf4c5a7af
Shiv <shiv@ubuntu.ubuntu-domain>
2012-12-13 09:39:39 +0530

    Program to flatten list

---
+++ list_flatten.py
@@ -0,0 +1,14 @@
+input=[1,2,[3,4,5],6,7,[8,[9,10],11],12]
+output=[]
+def flatten(input1,data=(list)):
+	def list(input1):
+		for value in input1:
+			if isinstance(value, data):
+				list(value)
+			else:    
+				output.append(value)
+	list(input1)
+	return output
+print flatten(input)
+ 
+
