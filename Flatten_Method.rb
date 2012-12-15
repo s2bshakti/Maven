bfd580cc8fea6141b4a691e84a6aa74d406f419c
Shiv <shiv@ubuntu.ubuntu-domain>
2012-12-15 22:51:27 +0530

    Refinement

--- Flatten_Method.rb
+++ Flatten_Method.rb
@@ -1,11 +1,11 @@
 inpList=[1,2,[3,4,5],6,7,[8,[9,10],11],12]
 def flatten(inpList,outList)
 	inpList.each do |inp_value|
-	if inp_value.kind_of?(Array)
-		flatten(inp_value,outList)
-	else
-		outList.push(inp_value)
-	end
+		if inp_value.kind_of?(Array)
+			flatten(inp_value,outList)
+		else
+			outList.push(inp_value)
+		end
 	end
 return outList
 end
